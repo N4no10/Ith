37f78e64b5abe0d5b77614607092bd34fb2fd4f0
arnaldop <arnaldop@eti.biocubafarma.cu>
2022-05-17 16:00:08 -0400

    -Se arreglaron los formatos de las fechas y se termino la funcionalidad de PedidosCancelados.

---
+++ app/src/main/java/cu/gob/ith/domain/interactors/GetFilterListPedidosCanceladosUseCase.java
@@ -0,0 +1,28 @@
+package cu.gob.ith.domain.interactors;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import cu.gob.ith.data.repository.ImplRepository;
+import cu.gob.ith.domain.mappers.TransformApiDatosPedidoToDatosPedido;
+import cu.gob.ith.domain.model.DatosPedido;
+import io.reactivex.rxjava3.core.Observable;
+
+public class GetFilterListPedidosCanceladosUseCase
+        implements GlobalUseCase<Observable<List<DatosPedido>>, Void> {
+
+    private final ImplRepository implRepository;
+
+    @Inject
+    public GetFilterListPedidosCanceladosUseCase(ImplRepository implRepository) {
+        this.implRepository = implRepository;
+    }
+
+    @Override
+    public Observable<List<DatosPedido>> execute() {
+        return implRepository.filterListPedidosCancelados()
+                .map(apiPedidoResponse ->
+                        TransformApiDatosPedidoToDatosPedido.mapListSecondConstructor(apiPedidoResponse.getApiDatosPedido()));
+    }
+}
